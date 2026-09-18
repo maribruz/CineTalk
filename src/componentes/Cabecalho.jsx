75677f85dfa88d5318f3b190065732c3ac69b443
maribruz <marianabruzadim07@gmail.com>
2026-09-18 09:17:10 -0300

    atualização do css

--- src/componentes/Cabecalho.jsx
+++ src/componentes/Cabecalho.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import { FaClapperboard } from "react-icons/fa6";
 
 function Cabecalho() {
   return (
     <header>
-      <Link to="/">CineTalk</Link>
+      <Link to="/">
+      <FaClapperboard /> CineTalk</Link>
       <nav>
         <Link to="/filmes">Filmes</Link>
         <Link to="/minhas-avaliacoes">Minhas avaliações</Link>
--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { buscarFilmesPopulares } from "../services/tmdb.js";
 
 function Inicio() {
+  const [filmesDestaque, setFilmesDestaque] = useState([]);
+
+  useEffect(() => {
+    buscarFilmesPopulares().then((filmes) => {
+      setFilmesDestaque(filmes.slice(0, 4));
+    });
+  }, []);
+
   return (
-    <div>
+    <div className="tela-inicial">
       <h1>CineTalk</h1>
-      <p>Avalie os filmes que você já viu e veja o que outras pessoas acharam.</p>
+      <p>Seu guia pessoal de cinema. Registre todos os filmes que você já viu, acompanhe suas avaliações e explore as opiniões da comunidade para encontrar a sua próxima grande recomendação.</p>
       <Link to="/filmes">Ver filmes</Link>
+
+      <div className="vitrine-destaque">
+        {filmesDestaque.map((filme) => (
+          <img
+            key={filme.id}
+            src={`https://image.tmdb.org/t/p/w342${filme.poster_path}`}
+            alt={filme.title}
+          />
+        ))}
+      </div>
     </div>
   );
 }
